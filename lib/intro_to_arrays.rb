94fe18a6decceab80ff52d88a9679223da68c67b
Maharshee roy <m.roy@optonline.net>
2018-11-04 20:43:31 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -12,6 +12,9 @@ def first_element(array)
   return array[0]
 end
 def third_element(array)
-  array = [1,2,3,4]
+  array = [1,2,"Style",4]
   return array[2]
 end
+def last_element(array)
+  array = [1,2,3,4]
+  return array[-1]
